490a2b292797c3e3e741b382257ef1f197fda809
xtl117 <1173918263@qq.com>
2022-10-11 18:06:37 +0800

    "test"

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -18,6 +18,7 @@ public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
         log.info("项目启动成功");
+        System.out.println("修改了一次代码");
 
     }
 }
